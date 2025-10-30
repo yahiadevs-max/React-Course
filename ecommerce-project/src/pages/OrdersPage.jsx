5863adac33c3919229773e4dc802f805b56d2da2
yahia <yahiadevs@gmail.com>
2025-10-30 15:06:04 +0000

    Split header into a component and use Link

--- ecommerce-project/src/pages/OrdersPage.jsx
+++ ecommerce-project/src/pages/OrdersPage.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router'
+import {Header} from '../Components/Header'
 import '../index.css'
 import './OrdersPage.css'
 
@@ -6,37 +8,7 @@ export function OrdersPage(){
     <>
     <title>Orders</title>
 
-        <div className="header">
-      <div className="left-section">
-        <a href="/" className="header-link">
-          <img className="logo"
-            src="images/logo-white.png" />
-          <img className="mobile-logo"
-            src="images/mobile-logo-white.png" />
-        </a>
-      </div>
-
-      <div className="middle-section">
-        <input className="search-bar" type="text" placeholder="Search" />
-
-        <button className="search-button">
-          <img className="search-icon" src="images/icons/search-icon.png" />
-        </button>
-      </div>
-
-      <div className="right-section">
-        <a className="orders-link header-link" href="/orders">
-
-          <span className="orders-text">Orders</span>
-        </a>
-
-        <a className="cart-link header-link" href="/checkout">
-          <img className="cart-icon" src="images/icons/cart-icon.png" />
-          <div className="cart-quantity">3</div>
-          <div className="cart-text">Cart</div>
-        </a>
-      </div>
-    </div>
+    <Header />
 
     <div className="orders-page">
       <div className="page-title">Your Orders</div>
@@ -84,11 +56,11 @@ export function OrdersPage(){
             </div>
 
             <div className="product-actions">
-              <a href="/tracking">
+              <Link to="/tracking">
                 <button className="track-package-button button-secondary">
                   Track package
                 </button>
-              </a>
+              </Link>
             </div>
 
             <div className="product-image-container">
@@ -112,11 +84,11 @@ export function OrdersPage(){
             </div>
 
             <div className="product-actions">
-              <a href="/tracking">
+              <Link to="/tracking">
                 <button className="track-package-button button-secondary">
                   Track package
                 </button>
-              </a>
+              </Link>
             </div>
           </div>
         </div>
@@ -163,11 +135,11 @@ export function OrdersPage(){
             </div>
 
             <div className="product-actions">
-              <a href="/tracking">
+              <Link to="/tracking">
                 <button className="track-package-button button-secondary">
                   Track package
                 </button>
-              </a>
+              </Link>
             </div>
           </div>
         </div>
