74d6140706bb58060fd072ec9041f9ea75748de1
yahia <yahiadevs@gmail.com>
2025-10-30 14:34:08 +0000

    update links to mutch router and move orders an tracking pages to react

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,5 +1,8 @@
 import { Routes, Route } from 'react-router'
-import {HomePage} from './pages/HomePage'
+import { HomePage } from './pages/HomePage'
+import { CheckoutPage } from './pages/CheckoutPage'
+import { OrdersPage } from './pages/OrdersPage'
+import { TrackingPage } from './pages/TrackingPage'
 import './App.css'
 
 function App() {
@@ -9,7 +12,9 @@ function App() {
     <>
       <Routes>
         <Route index element={<HomePage />} />
-        <Route path="checkout" element={<div>test checkout page</div>} />
+        <Route path="checkout" element={<CheckoutPage />} />
+        <Route path="orders" element={<OrdersPage />} />
+        <Route path="tracking" element={<TrackingPage />} />
       </Routes>
     </>
  
