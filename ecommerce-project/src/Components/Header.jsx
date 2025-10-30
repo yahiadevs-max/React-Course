f6ab9895b97a5efb411530a9ea0a89450ba013c9
yahia <yahiadevs@gmail.com>
2025-10-30 19:16:48 +0000

    Get cart from the backend and display quantity

--- ecommerce-project/src/Components/Header.jsx
+++ ecommerce-project/src/Components/Header.jsx
@@ -7,8 +7,14 @@ import CartIcon from '../assets/images/icons/cart-icon.png';
 
 import './header.css';
 
-export function Header(){
+export function Header( {cart} ){
 
+      let totalQuantity = 0;
+
+      cart.forEach((cartItem) => {
+        totalQuantity += cartItem.quantity;
+      });
+   
     return (
     <>  
       <div className="header">
@@ -37,7 +43,7 @@ export function Header(){
 
           <NavLink className="cart-link header-link" to="/checkout">
             <img className="cart-icon" src={CartIcon} />
-            <div className="cart-quantity">3</div>
+            <div className="cart-quantity">{totalQuantity}</div>
             <div className="cart-text">Cart</div>
           </NavLink>
         </div>
--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -9,19 +9,23 @@ import './HomePage.css'
 
 export function HomePage() {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
           axios.get('http://localhost:3000/api/products').then((response) => {
               setProducts(response.data);   
               });
+          axios.get('http://localhost:3000/api/cart-items').then((response) => {
+              setCart(response.data);  
+              });          
             }, []);
-
+  
   return (
     <>
       <title>Ecommerce Project</title>
       <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
 
-      <Header />
+      <Header cart ={cart}/>
 
       <div className="home-page">
         <div className="products-grid">
