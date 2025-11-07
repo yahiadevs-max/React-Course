cb560d8ee707fd7d36680efa4052dfed13242cbc
yahia <yahiadevs@gmail.com>
2025-11-07 09:09:59 +0000

    Use beforeEach to remove duplication

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect, describe, vi } from 'vitest';
+import { it, expect, describe, vi, beforeEach } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import axios from 'axios';
@@ -7,10 +7,11 @@ import { Product } from './Product';
 vi.mock('axios');
 
 describe('Product component', () => {
+    let product ;
+    let loadCart;
 
-  it('displays the product details correctly', () => {
-
-    const product =   {
+  beforeEach(() => {
+    product = {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -22,33 +23,22 @@ describe('Product component', () => {
     keywords: ["socks", "sports", "apparel"]
   };
 
-  const loadCart = vi.fn();
-   
-    render(<Product product={product} loadCart={loadCart}/>);
+    loadCart = vi.fn();
+    render(<Product product={product} loadCart={loadCart} />);
+  });
+
+  it('displays the product details correctly', () => {
 
     expect(screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')).toBeInTheDocument();
     expect(screen.getByText('$10.90')).toBeInTheDocument();
     expect(screen.getByTestId('product-image')).toHaveAttribute('src', 'images/products/athletic-cotton-socks-6-pairs.jpg');
     expect(screen.getByTestId('product-rating-stars-image')).toHaveAttribute('src', 'images/ratings/rating-45.png');
     expect(screen.getByText('87')).toBeInTheDocument();
+
   });
 
-    it('adds a product to the cart', async() => {
-        const product =   {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: ["socks", "sports", "apparel"]
-  };
+  it('adds a product to the cart', async () => {
 
-  const loadCart = vi.fn();
-   
-    render(<Product product={product} loadCart={loadCart}/>);
     const user = userEvent.setup();
     const addToCartButton = screen.getByTestId('add-to-cart-button');
     await user.click(addToCartButton);
@@ -58,6 +48,6 @@ describe('Product component', () => {
     });
     expect(loadCart).toHaveBeenCalled();
 
-    });
+  });
 
 });
